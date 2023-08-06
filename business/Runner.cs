0c6b38ebdfef3eaad8c782e5bc1bf8d1028255bb
Deshy Dan <deshy733@gmail.com>
2023-08-06 21:43:10 +0200

    Added runner class

---
+++ business/Runner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaiteringSystem.business
+{
+    public class Runner:Role
+    {
+
+        private int tips;
+        private int rate;
+        private int noOfShifts;
+        public Runner(int tips, int rate, int noOfShifts) : base("Runner", RoleType.Runner)
+        {
+            this.tips = tips;
+            this.rate = rate;
+            this.noOfShifts = noOfShifts;
+        }
+
+
+
+
+        public override int payment()
+        {
+            return rate * noOfShifts;
+        }
+
+        public override int payment(int tips)
+        {
+            return rate * noOfShifts + tips;
+        }
+
+
+    }
+}
