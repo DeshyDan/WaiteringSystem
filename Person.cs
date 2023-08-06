433fb7b097c64eb7aa36af235121d397d387dc46
Deshy Dan <deshy733@gmail.com>
2023-08-06 21:25:31 +0200

    Added person class

---
+++ Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaiteringSystem
+{
+    public abstract class Person
+    {
+        #region Data Members
+        private string ID;
+        private string name;
+        private string phone;
+        #endregion
+        #region Constructor
+        public Person(string ID, string name, string phone)
+        {
+
+            this.ID = ID;
+            this.name = name;
+            this.phone = phone;
+        }
+        #endregion
+
+        #region Property Methods
+        public string getID()
+        {
+            return this.ID;
+        }
+        public void setID(string ID)
+        {
+            this.ID = ID;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+        public string getPhone()
+        {
+            return this.phone;
+        }
+
+        public void setPhone(string phone)
+        {
+            this.phone = phone;
+        }
+        #endregion
+
+
+    }
+}
