fa6142900ce9771d85f63b051bb221de061c4610
Deshy Dan <deshy733@gmail.com>
2023-08-06 21:50:51 +0200

    Added headWaiter class

---
+++ business/HeadWaiter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaiteringSystem.business
+{
+    public class HeadWaiter : Role
+    {
+        private int salary;
+        public HeadWaiter() : base("HeadWaiter", RoleType.Headwaiter)
+        {
+            this.salary = 0;
+        }
+        public override int payment(int amount)
+        {
+            return salary;
+        }
+      
+        public int getSalary()
+        {
+            return salary;
+        }
+        public void setSalary(int salary)
+        {
+            this.salary = salary;   
+        }
+    }
+}
