df18c95f43bdd34a162d2885456b1cc63adc12aa
Deshy Dan <deshy733@gmail.com>
2023-08-06 23:15:46 +0200

    Added employee Class

---
+++ business/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WaiteringSystem;
+
+namespace WaiteringSystem.business
+{
+    public class Employee : Person
+    {
+        private string number;
+        public Role role;
+
+        public Employee(Role roleValue ):base() {
+            var roleType = roleValue.getRole();
+            switch (roleType) {
+
+                case RoleType.noRole:
+                        role = new Role();
+                    break;
+                case RoleType.Headwaiter:
+                    role = new HeadWaiter();
+                    break;
+                case RoleType.Waiter:
+                    role = new Waiter();
+                      break;
+                case RoleType.Runner:
+                    role = new Runner();
+                      break;
+                default:
+                    throw new Exception("Invalid Role Type was given");
+
+            }
+
+        }
+        public string getNumber( )
+        {
+            return number;
+        }
+
+        public Role getRole( )
+        {
+            return role;
+        }
+
+        public void setRole( Role roleValue )
+        {
+            this.role = roleValue;
+        }
+
+       
+    }
+}
