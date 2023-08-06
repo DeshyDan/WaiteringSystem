a714281041aace1d0dc4e86da6d6fc186eafd4d2
Deshy Dan <deshy733@gmail.com>
2023-08-06 21:46:54 +0200

    Added waiter class

---
+++ business/Waiter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaiteringSystem.business
+{
+    public class Waiter:Role
+    {
+        private int tips;
+        private int rate;
+        private int noOfShifts;
+        public Waiter(int tips, int rate, int noOfShifts)
+        {
+            this.tips = tips;
+            this.rate = rate;
+            this.noOfShifts = noOfShifts;
+        }
+        public override int payment(int tips)
+        {
+            return rate * noOfShifts + tips;
+        }
+
+    }
+}
