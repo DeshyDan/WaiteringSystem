36cf26bba4ed4689f288fff36d636acb3d8c6ea1
Deshy Dan <deshy733@gmail.com>
2023-08-06 23:16:17 +0200

    Added waiter class

--- business/Waiter.cs
+++ business/Waiter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,19 +9,38 @@ namespace WaiteringSystem.business
 {
     public class Waiter:Role
     {
-        private int tips;
+        #region instance vars
+        private int tips; 
         private int rate;
         private int noOfShifts;
-        public Waiter(int tips, int rate, int noOfShifts)
+        #endregion
+        #region Constructor
+        public Waiter( ):base("Waite", RoleType.Waiter)
         {
-            this.tips = tips;
-            this.rate = rate;
-            this.noOfShifts = noOfShifts;
+            this.tips = 0;
+            this.rate = 0;
+            this.noOfShifts = 0;
+        }
+        #endregion
+        #region Properties
+
+        public int getRate() { return rate; }
+        public void setRate(int rate) { this.rate = rate; }
+        public int getTips() { return tips; }
+        public void setTips(int tips) {  this.tips = tips; }
+        public int getNoOfShifts()
+        {
+            return noOfShifts;
+        }
+        public void setNoOfShifts(int noOfShift)
+        {
+            noOfShifts=noOfShift;
         }
         public override int payment(int tips)
         {
             return rate * noOfShifts + tips;
         }
+        #endregion
 
     }
 }
