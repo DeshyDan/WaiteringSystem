ad82f5bf3a7929fa79a912069527f68a3ee1de1a
Deshy Dan <deshy733@gmail.com>
2023-08-06 23:16:07 +0200

    Added runner class

--- business/Runner.cs
+++ business/Runner.cs
@@ -8,29 +8,49 @@ namespace WaiteringSystem.business
 {
     public class Runner:Role
     {
-
+        #region instance varsd
         private int tips;
         private int rate;
         private int noOfShifts;
-        public Runner(int tips, int rate, int noOfShifts) : base("Runner", RoleType.Runner)
+        #endregion
+
+        #region constructors
+        public Runner() : base("Runner", RoleType.Runner)
         {
-            this.tips = tips;
-            this.rate = rate;
-            this.noOfShifts = noOfShifts;
+           this.tips                = 0;
+            this.rate = 0;
+            this.noOfShifts = 0;
         }
+        #endregion
 
-
-
-
+        #region properties
         public override int payment()
         {
             return rate * noOfShifts;
         }
 
+
         public override int payment(int tips)
         {
             return rate * noOfShifts + tips;
         }
+        public int getTips()
+        {
+            return tips;
+        }
+        public void setTips(int tips)
+        {
+            this.tips= tips;
+        }
+        public int getRate()
+        {
+            return rate;
+        }
+        public int getNoOfShifts()
+        {
+            return noOfShifts;
+        }
+        #endregion
 
 
     }
